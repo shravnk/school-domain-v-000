a76a967220fc3b883f27b59fd029d841f562c187
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 02:36:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+
+  def add_student(name, grade)
+    @roster[grade] = name
+  end
+
+
+
+end
