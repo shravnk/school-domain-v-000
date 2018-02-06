aa523154ebe263f79bd4535ef1f86fb463ff8a2d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 02:42:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,13 @@ class School
 
 
   def add_student(name, grade)
-    @roster[grade] = name
+    if @roster.key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+    
   end
 
 
